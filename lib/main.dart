ab9420b604be127f3753b922c397aa81204e61e2
sgosiaco <sgosiaco@me.com>
2020-10-28 07:28:19 -0700

    Finally implemented basic song history that is loaded  between launches. Using isolates to do the conversion to MediaItem and also sort by lastModifiedSync. Added path_provider and path for getting app documents directory.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:isolate';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:audio_service/audio_service.dart';
-
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 import 'package:saisei/AudioPlayerTask.dart';
 import 'package:saisei/SongList.dart';
 import 'package:saisei/ControlSheet.dart';
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -47,7 +54,6 @@ class Player extends StatefulWidget {
 class _PlayerState extends State<Player> {
   final player = AudioPlayer();
   List<MediaItem> _songs;
-  String _message = '';
 
   Future<bool> startAudioService() {
     return AudioService.start(
@@ -62,6 +68,7 @@ class _PlayerState extends State<Player> {
     // TODO: implement initState
     super.initState();
     startAudioService();
+    loadSongs();
   }
 
   @override
@@ -82,10 +89,103 @@ class _PlayerState extends State<Player> {
     ));
   }
 
-  void sortSongList(DateTime date) {    
-    print('Starting reg sort');
-    //_songs.sort((a,b) => int.parse(b.id).compareTo(int.parse(a.id))); // recently added hack
-    _songs.sort((a,b) => File(b.id).lastModifiedSync().compareTo(File(a.id).lastModifiedSync()));
+  Widget _buildPlayer() {
+    return Builder(builder: (context) {
+      if ((_songs ?? []).length == 0) {
+        return Center(child: CircularProgressIndicator());
+      }
+        return Scrollbar(child: SongList(songs: _songs));
+    });
+  }
+
+  Future<void> loadSongs() async {
+    // check if local history exists and load it
+    final dir = await getApplicationDocumentsDirectory();
+    print(dir);
+    final songFile = File(p.join(dir.path, 'songs.json'));
+    List<MediaItem> songsJson;
+    if (await songFile.exists()) {
+      print('FILE EXISTS');
+      songsJson = (jsonDecode(await songFile.readAsString()) as List).map((song) => MediaItem.fromJson(song)).toList();
+      setState(() {
+        _songs = songsJson;
+      });
+    } 
+
+    // task first isolate with getting list
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn(songLoader, receivePort.sendPort);
+    SendPort sendPort = await receivePort.first;
+    
+    final songsInfo = await FlutterAudioQuery().getSongs();
+    final songSerialized = songsInfo.map((song) => {
+      'id': song.filePath,
+      'album': song.album,
+      'title':  song.title,
+      'artist': song.artist,
+      'duration': song.duration,
+      'artUri': song.albumArtwork,
+      'albumId': song.albumId,
+      'artistId': song.artistId,
+      'composer': song.composer,
+      'fileSize': song.fileSize,
+      'track': song.track,
+      'uri': song.uri,
+      'year': song.year
+    }).toList();
+
+    List<Map<String, dynamic>> msg = await sendReceive(sendPort, songSerialized);
+    final current = msg.map((e) => MediaItem.fromJson(e)).toList();
+
+    setState(() {
+      _songs = current;
+    });
+
+    if (!await songFile.exists() || !listEquals(songsJson, current)) {
+      print('WRITING FILE');
+      songFile.writeAsString(jsonEncode(msg));
+    }
+  }
+
+  static Future<void> songLoader(SendPort sendPort) async {
+    ReceivePort port = ReceivePort();
+    sendPort.send(port.sendPort);
+    
+    await for (var msg in port) {
+      print('ISOLATE RECEIVED');
+      SendPort replyTo = msg[1];
+      final songsMedia = (msg[0] as List<Map<String, dynamic>>).map((song) => MediaItem(
+                  id: song['id'],
+                  album: song['album'],
+                  title: song['title'],
+                  artist: song['artist'],
+                  genre: '',
+                  duration: Duration(milliseconds: int.parse(song['duration'])),
+                  artUri: song['artUri'] == null ? null : Uri.file(song['artUri'], windows: false).toString(),
+                  rating: Rating.newHeartRating(false),
+                  extras: {
+                    'albumId' : song['albumId'],
+                    'artistId' : song['artistId'],
+                    'composer' : song['composer'],
+                    'fileSize' : song['fileSize'],
+                    'track' : song['track'],
+                    'uri' : song['uri'],
+                    'year' : song['year'],
+                  }
+                ).toJson()
+              ).toList();
+      songsMedia.sort((a,b) => File(b['id']).lastModifiedSync().compareTo(File(a['id']).lastModifiedSync()));
+      replyTo.send(songsMedia);
+    }
+  }
+
+  Future sendReceive(SendPort port, msg) {
+    ReceivePort response = ReceivePort();
+    port.send([msg, response.sendPort]);
+    return response.first;
+  }
+
+  List<MediaItem> recentSongList(DateTime date) {        
     var left = 0;
     var right = _songs.length - 1;
     var middle = (left + right) ~/ 2;
@@ -101,59 +201,8 @@ class _PlayerState extends State<Player> {
       }
       middle = (left + right) ~/ 2;
     }
-    // TODO: commented out for emulator!!!!!!
-    _songs = _songs.getRange(0, middle + 1).toList();
     print('Final size ${_songs.length}');
-  }
-
-  Widget _buildPlayer() {
-    return Stack(children: [
-      FutureBuilder(
-        future: FlutterAudioQuery().getSongs(),
-        builder: (BuildContext context, AsyncSnapshot<List<SongInfo>> snapshot) {
-          Widget child;
-          if (snapshot.hasData) {
-            _songs = snapshot.data.map((song) => MediaItem(
-                id: song.filePath,
-                album: song.album,
-                title: song.title,
-                artist: song.artist,
-                genre: '',
-                duration: Duration(milliseconds: int.parse(song.duration)),
-                artUri: song.albumArtwork == null ? null : Uri.file(song.albumArtwork, windows: false).toString(),
-                rating: Rating.newHeartRating(false),
-                extras: {
-                  'albumId' : song.albumId,
-                  'artistId' : song.artistId,
-                  'composer' : song.composer,
-                  'fileSize' : song.fileSize,
-                  'track' : song.track,
-                  'uri' : song.uri,
-                  'year' : song.year,
-                }
-              )
-            ).toList();
-            sortSongList(new DateTime.utc(2016, 9, 1));
-            child = SongList(songs: _songs);
-          } else {
-            _message = 'Loading';
-            if (snapshot.hasError) {
-              _message = 'Error';
-            }
-            child = Text(
-              _message,
-              style: TextStyle(
-                fontSize: 30
-              ),
-            );
-          }
-          return Container(
-            //height: 526,
-            child: Scrollbar(
-              child: child,
-            ),
-          );
-      }),
-    ]);
+    return _songs.getRange(0, middle + 1).toList();
+    
   }
 }
